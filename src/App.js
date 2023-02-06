77110a111aa3f47eb1881c4b181e225fc2e5a2f6
Ernest Wambua <ernestwambua2@gmail.com>
2023-02-06 11:59:12 +0300

    Done

--- src/App.js
+++ src/App.js
@@ -5,6 +5,24 @@ const api = axios.create({ baseURL: process.env.REACT_APP_BASE_URL });
 
 export const App = () => {
 	const [todos, setTodos] = useState([]);
+	const [title, setTitle] = useState('');
+	const [flag, setFlag] = useState(false);
+
+	const createTodo = async (title) => {
+		await api.post('/api/todo', { title });
+		setTitle('');
+		setFlag(!flag);
+	};
+
+	const editTodo = async (id, data) => {
+		await api.patch(`/api/todo/${id}`, data);
+		setFlag(!flag);
+	};
+
+	const deleteTodo = async (id) => {
+		await api.delete(`/api/todo/${id}`);
+		setFlag(!flag);
+	};
 
 	useEffect(() => {
 		(async () => {
@@ -15,12 +33,12 @@ export const App = () => {
 				console.error(error);
 			}
 		})();
-	}, []);
+	}, [flag]);
 
 	return (
 		<div className='w-full h-full bg-gray-200 p-10'>
 			<div className='flex items-center justify-center'>
-				<div className='w-1/2 space-y-2'>
+				<div className='w-full sm:w-1/2 space-y-2'>
 					<div className='space-y-6 p-5 bg-white rounded-lg shadow-md'>
 						<h1 className='text-3xl font-medium text-gray-700 text-center'>
 							Todo App
@@ -31,18 +49,28 @@ export const App = () => {
 							{todos.map((todo, idx) => (
 								<li
 									key={idx}
-									className='flex justify-between items-center p-3 shadow-md rounded-md'
+									className={`flex justify-between items-center p-3 shadow-md rounded-md ${
+										todo.done ? 'bg-gray-200' : ''
+									}`}
 								>
-									<p>{todo.title}</p>
+									<p className={todo.done ? 'line-through' : ''}>
+										{todo.title}
+									</p>
 									<div className='flex items-center space-x-2'>
 										<input
 											className='h-7 w-7 rounded-full'
 											type='checkbox'
 											name='done'
-											checked={todo.done}
+											onChange={(e) =>
+												editTodo(todo._id, { done: e.target.checked })
+											}
+											defaultChecked={todo.done}
 										/>
-										<button className='px-2 py-1 bg-red-500 hover:bg-red-600 rounded-md text-white'>
-											<i class='fa-solid fa-trash'></i>
+										<button
+											className='px-2 py-1 bg-red-500 hover:bg-red-600 rounded-md text-white'
+											onClick={() => deleteTodo(todo._id)}
+										>
+											<i className='fa-solid fa-trash'></i>
 										</button>
 									</div>
 								</li>
@@ -53,8 +81,13 @@ export const App = () => {
 						<input
 							className='h-10 text-lg w-full px-3 border-2 border-gray-400 rounded-md focus:outline-none text-gray-700'
 							type='text'
+							value={title}
+							onChange={(e) => setTitle(e.target.value)}
 						/>
-						<button className='w-full p-2 bg-green-500 hover:bg-green-600 rounded-md shadow-md text-white text-2xl font-bold'>
+						<button
+							className='w-full p-2 bg-green-500 hover:bg-green-600 rounded-md shadow-md text-white text-2xl font-bold'
+							onClick={() => createTodo(title)}
+						>
 							Add
 						</button>
 					</div>
